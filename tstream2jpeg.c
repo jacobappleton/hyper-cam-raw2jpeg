7d154244835480b793c73370bb0caec3303c76e2
Jacob Appleton <jacob.appleton@uqconnect.edu.au>
2012-11-14 09:25:14 +1000

    Updated code headers

--- tstream2jpeg.c
+++ tstream2jpeg.c
@@ -1,5 +1,5 @@
 // hyper-cam - Interfaces camera sensor with serial communications for
-// autonomous image capture of scientific experiments on an embedded Linux
+// autonomous image capture of scientific experiments on embedded Linux
 //
 // Image processing functions.
 //
@@ -16,15 +16,15 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with this program.  If not, see <www.gnu.org/licenses/>.
 //
 // This software was developed as part of the Scramspace I Flight Experiment,
 // funded by the Australian Space Research Program -
 // http://www.space.gov.au/AUSTRALIANSPACERESEARCHPROGRAM and involving:
 // - The University of Queensland (UQ) - www.uq.edu.au
 // - Australian Government Department of Defence - Defence Science and
-//   Technology Organisation (DSTO) - http://www.dsto.defence.gov.au
-// - German Aerospace Center (DLR) - http://www.dlr.de/en/
+//   Technology Organisation (DSTO) - www.dsto.defence.gov.au
+// - German Aerospace Center (DLR) - www.dlr.de/en/
 // - University of Southern Queensland (USQ) - www.usq.edu.au
 // - BAE Systems - www.baesystems.com
 // - Japan Aerospace Exploration Agency (JAXA) - www.jaxa.jp/index_e.html
@@ -33,7 +33,7 @@
 // - Australian Youth Aerospace Association (AYAA) - www.ayaa.com.au
 // - Centro Italiano Ricerche Aerospaziali (CIRA) - www.cira.it/en
 // - The University of Adelaide - www.adelaide.edu.au
-// - Teakle Composites - http://www.cira.colostate.edu
+// - Teakle Composites - www.cira.colostate.edu
 // - The University of New South Wales (UNSW) - www.unsw.edu.au/
 //
 // Converts a stream of video data from the Scramspace Flight Experiment to a
